40211e661c943593c1b2752fd6a9daabfd94b9ae
AMIR MARJANI <amir.marjani34@gmail.com>
2021-09-13 11:31:31 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import sbLogo from './assets/sb-logo-giris.png';
-import enabiz from './assets/enabiz-logo-giris-eng.png'
-import checkmark from './assets/checkmark.png'
+
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import MaryamPage from './pages/maryam/maryam.component';
+import AmirPage from './pages/amir/amir.component';
 
 function App() {
   return (
-    <div className="BoxContent">
-      <img src={sbLogo} className="SbLogo" alt="sbLogo"/>
-      <img src={enabiz} className="EnabizLogo" alt="enabizLogo"/>
-      <div className="clearfix"></div>
-      <div className="p-5">
-        <p className="note note-success">
-      <img src={checkmark} className="checkmark" alt="enabizLogo"/>
-          
-          The result of this test belongs to  A*** MARJANIBEN*** with Passport Number *******7018.
-          The test result is NEGATIVE and the sampling date is 13/9/2021 11:13:00 AM.<br/>
-          It was downloaded by  A*** MARJANIBEN*** on 13.09.2021.<br/>
-          CAN BE USED AS A DOCUMENT FOR EXIT ABROAD.<br/>
-        </p>
-        <div className="clearfix"></div>
-        <div className="clearfix"></div>
-      </div>
+    <div>
+      <Switch>
+        <Route path='/enabiz.gov.tr/Account-PcrTestSonucuDogrula-barcode-8be88bb2-7f4e-4afc-a603-9fe6b0adccba' component={MaryamPage} />
+        <Route path='/enabiz.gov.tr/Account-PcrTestSonucuDogrula-barcode-8be88bb2-7f4e-4afc-a603-9fe6b0adccbb' component={AmirPage} />
+      </Switch>
     </div>
 
   );
 }
 
 export default App;
-
-// The result of this test belongs to  A *** MARJANIBEN *** with Passport Number ******* 7018.
-//         The test result is NEGATIVE and the sampling date is 7 / 2 / 2021 11: 13: 00 AM.
-//         < br >
-//   It was downloaded by  A *** MARJANIBEN *** on 02.07.2021.
-//         < br >
-//     CAN BE USED AS A DOCUMENT FOR EXIT ABROAD.
---
+++ src/pages/maryam/maryam.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MaryamPage = () => (
+    <div className="MaryamPage">
+        aaaaaa
+    </div>
+)
+
+export default MaryamPage;
